9c337dbf5757d280b20294b29245f278a9ce3630
Nicolas-Saavedra <personal@nicolassaavedra.com>
2025-04-07 14:29:04 -0500

    refactor: format acording to prettier with editconf

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,15 +17,30 @@ export default function App() {
 
   return (
     <div className="relative w-screen h-screen">
-      {automata && <ForceGraph nodes={automata.nodes} links={automata.links} initialNode={automata.initial_node} finalNodes={automata.final_nodes} />}
+      {automata && (
+        <ForceGraph
+          nodes={automata.nodes}
+          links={automata.links}
+          initialNode={automata.initial_node}
+          finalNodes={automata.final_nodes}
+        />
+      )}
 
       <div className="absolute z-10 flex flex-col w-full h-full justify-between pointer-events-none">
         <div className="flex justify-between">
           <a href="#" className="pointer-events-auto">
-            <img src="/pyGold.svg" className="h-16 px-4 py-2 m-4 bg-gray-100 rounded-2xl" alt="pyGold Logo" />
+            <img
+              src="/pyGold.svg"
+              className="h-16 px-4 py-2 m-4 bg-gray-100 rounded-2xl"
+              alt="pyGold Logo"
+            />
           </a>
           <div className="flex items-center">
-            <input type="text" className="bg-gray-100 text-gray-700 tracking-widest font-mono rounded-2xl w-80 h-16 m-4 px-8 pointer-events-auto" placeholder="Enter input..." />
+            <input
+              type="text"
+              className="bg-gray-100 text-gray-700 tracking-widest font-mono rounded-2xl w-80 h-16 m-4 px-8 pointer-events-auto"
+              placeholder="Enter input..."
+            />
             <button className="flex items-center justify-center bg-gray-100 rounded-2xl w-16 h-16 mr-4 px-8 pointer-events-auto hover:cursor-pointer">
               <StepForward className="min-w-12 text-gray-600" />
             </button>
@@ -37,10 +52,8 @@ export default function App() {
             </button>
           </div>
         </div>
-        <div className="flex">
-        </div>
+        <div className="flex"></div>
       </div>
-    </div >
+    </div>
   );
 }
-
